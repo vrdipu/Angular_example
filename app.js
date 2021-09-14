071bdca92956b3b08850e5f325130930c70adab1
vrdipu <vrdipu@gmail.com>
2021-09-14 12:25:59 +0530

    changed

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ angular.module('sortApp', [])
   
   // create the list of sushi rolls 
   $scope.sushi = [
-    { name: 'Cali Roll', fish: 'catfish', tastiness: 2 },
-    { name: 'Philly', fish: 'Tuna', tastiness: 4 },
-    { name: 'Tiger', fish: 'Eel', tastiness: 7 },
+    { name: 'Cali Roll', fish: 'Ashis', tastiness: 2 },
+    { name: 'Philly', fish: 'Dipu', tastiness: 4 },
+    { name: 'Tiger', fish: 'Arun', tastiness: 7 },
     { name: 'Rainbow', fish: 'Variety', tastiness: 6 },
     
     { name: 'Philly1', fish: 'Tuna1', tastiness: 41 },
